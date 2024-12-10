8fd38ce5721538a819c8c0b9428c45bb20f6becd
djaafarhamri <hamridjaafar12@gmail.com>
2024-12-10 13:43:11 +0100

    added captured pieces evaluation

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -154,6 +154,34 @@ const Game: React.FC = () => {
     { color: string; piece: string | undefined }[]
   >([]); // Store promoted pieces
 
+  const pieceValues: { [key: string]: number } = {
+    p: 1,
+    n: 3,
+    b: 3,
+    r: 5,
+    q: 9,
+  };
+
+  function calculateEvaluation(
+    blackCaptures: string[],
+    whiteCaptures: string[],
+    forPlayer: string
+  ): number {
+    // Calculate total value of pieces captured by each side
+    const blackScore = blackCaptures.reduce(
+      (sum, capture) => sum + pieceValues[capture],
+      0
+    );
+
+    const whiteScore = whiteCaptures.reduce(
+      (sum, capture) => sum + pieceValues[capture],
+      0
+    );
+
+    if (forPlayer === "white") return whiteScore - blackScore;
+    else return blackScore - whiteScore;
+  }
+
   useEffect(() => {
     const handleMoveMade = ({
       player,
@@ -294,9 +322,9 @@ const Game: React.FC = () => {
         console.log("nigga : ", result.color, result.captured);
         const capturedPiece = wasPromotedPiece ? "p" : result.captured; // Show as pawn if promoted
         if (wasPromotedPiece) {
-          console.log("was promoted")
-          console.log("was promoted promoted pieces: ", promotedPieces)
-          console.log("was promoted result: ", result)
+          console.log("was promoted");
+          console.log("was promoted promoted pieces: ", promotedPieces);
+          console.log("was promoted result: ", result);
           setPromotedPieces(
             (prev: { color: string; piece: string | undefined }[]) => {
               const indexToRemove = prev.findIndex(
@@ -309,13 +337,13 @@ const Game: React.FC = () => {
                 console.log("gla3nah: ", [
                   ...prev.slice(0, indexToRemove),
                   ...prev.slice(indexToRemove + 1),
-                ])
+                ]);
                 return [
                   ...prev.slice(0, indexToRemove),
                   ...prev.slice(indexToRemove + 1),
                 ];
               }
-              console.log("magla3nahch: ", prev)
+              console.log("magla3nahch: ", prev);
               return prev; // No match found, return the original array
             }
           );
@@ -418,6 +446,9 @@ const Game: React.FC = () => {
                 blackCaptures={blackCaptures}
                 isWhite={false}
               />
+              {(calculateEvaluation(whiteCaptures, blackCaptures, "white") > 0) && (
+              <p className="text-center text-white m-2">+{calculateEvaluation(whiteCaptures, blackCaptures, "white")}</p>
+              )}
               <div className="h-10 w-10 bg-white ml-auto"></div>
             </div>
             <div className="text-xl  font-bold text-yellow-400 py-2 px-8 bg-[#313131]">
@@ -432,6 +463,9 @@ const Game: React.FC = () => {
                 blackCaptures={blackCaptures}
                 isWhite={true}
               />
+              {(calculateEvaluation(whiteCaptures, blackCaptures, "black") > 0) && (
+              <p className="text-center text-white m-2">+{calculateEvaluation(whiteCaptures, blackCaptures, "black")}</p>
+              )}
               <div className="h-10 w-10 bg-black ml-auto"></div>
             </div>
             <div className="text-xl  font-bold text-yellow-400 py-2 px-8 bg-[#313131]">
@@ -456,7 +490,10 @@ const Game: React.FC = () => {
                 blackCaptures={blackCaptures}
                 isWhite={false}
               />
-              <div className="h-10 w-10 bg-white ml-auto"></div>
+              {(calculateEvaluation(whiteCaptures, blackCaptures, "white") > 0) && (
+                <p className="text-center text-white m-2">+{calculateEvaluation(whiteCaptures, blackCaptures, "white")}</p>
+              )}
+                <div className="h-10 w-10 bg-white ml-auto"></div>
             </div>
             <div className="text-xl  font-bold text-yellow-400 py-2 px-8 bg-[#313131]">
               <h2>{typeof game?.white !== "string" && game?.white.username}</h2>
@@ -470,6 +507,9 @@ const Game: React.FC = () => {
                 blackCaptures={blackCaptures}
                 isWhite={true}
               />
+              {(calculateEvaluation(whiteCaptures, blackCaptures, "black") > 0) && (
+              <p className="text-center text-white m-2">+{calculateEvaluation(whiteCaptures, blackCaptures, "black")}</p>
+              )}
               <div className="h-10 w-10 bg-black ml-auto"></div>
             </div>
             <div className="text-xl  font-bold text-yellow-400 py-2 px-8 bg-[#313131]">
