97fb539e17eb5fa8e1b375ab24b73c21400c70f5
djaafarhamri <hamridjaafar12@gmail.com>
2024-12-12 13:24:41 +0100

    quick pairing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ import OnlineUsers from "../components/home/OnlineUsers";
 
 const Home = () => {
   const { user } = useUser();
+  const [activeTab, setActiveTab] = useState("onlineUsers"); // Track active tab
   const [onlineUsers, setOnlineUsers] = useState<string[]>([]);
   const [challenger, setChallenger] = useState<string | null>(null);
   const [opponant, setOpponant] = useState<string>("");
@@ -15,32 +16,72 @@ const Home = () => {
   const [timer, setTimer] = useState("10 min");
   const navigate = useNavigate();
   const socket = useContext(SocketContext);
-  const handleChallenge = (opponent: string, timer: string) => {
-    socket.emit("sendChallenge", {
-      challenger: user?.username,
-      opponent,
-      timer,
-    });
-  };
-  const handleAccept = async (timer: string) => {
-    const response = await fetch("http://localhost:4000/api/game/create", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ opponant: user?.username, challenger, timer }),
-      credentials: "include",
-    });
 
-    const data = await response.json();
+  interface TimerOption {
+    label: string;
+    value: string;
+  }
+
+  interface TimerCategory {
+    name: string;
+    options: TimerOption[];
+  }
+  const timerCategories: TimerCategory[] = [
+    {
+      name: "Bullet",
+      options: [
+        { label: "1 min", value: "1+0" },
+        { label: "1 | 1", value: "1+1" },
+        { label: "2 | 1", value: "2+1" },
+        { label: "2 | 2", value: "2+2" },
+        { label: "30 sec | 1", value: ".5+1" },
+      ],
+    },
+    {
+      name: "Blitz",
+      options: [
+        { label: "3 min", value: "3+0" },
+        { label: "3 | 2", value: "3+2" },
+        { label: "5 min", value: "5+0" },
+        { label: "5 | 2", value: "5+2" },
+        { label: "5 | 5", value: "5+5" },
+      ],
+    },
+    {
+      name: "Rapid",
+      options: [
+        { label: "10 min", value: "10+0" },
+        { label: "15 | 10", value: "15+10" },
+        { label: "10 | 5", value: "10+5" },
+        { label: "20 min", value: "20+0" },
+        { label: "60 min", value: "60+0" },
+      ],
+    },
+  ];
+
+  const handleQuickPair = (timer: string) => {
+    socket.emit("quickPairing", { username: user?.username, timer });
+  };
+
+  const handleAccept = async (timer: string) => {
     socket.emit("acceptChallenge", {
       challenger,
       opponent: user?.username,
-      game: data.game._id,
+      timer,
     });
     setChallenger(null);
   };
 
   const handleDecline = () => {
     setChallenger(null);
+  };
+
+  const handleChallenge = (opponent: string, timer: string) => {
+    socket.emit("sendChallenge", {
+      challenger: user?.username,
+      opponent,
+      timer,
+    });
   };
 
   useEffect(() => {
@@ -52,8 +93,7 @@ const Home = () => {
   useEffect(() => {
     const handleOnlineUsers = (users: string[]) => {
       setOnlineUsers(users);
-    }
-    // Listen for updated online user list
+    };
     socket.on("onlineUsers", handleOnlineUsers);
     return () => {
       socket.off("onlineUsers", handleOnlineUsers);
@@ -61,41 +101,136 @@ const Home = () => {
   }, [socket]);
 
   useEffect(() => {
-    const handleStartGame = (game: {id: string}) => {
-      navigate("/game/" + game.id);
-    }
-    // Listen for updated online user list
+    const handleStartGame = ({ gameId }: { gameId: string }) => {
+      navigate("/game/" + gameId);
+    };
     socket.on("startGame", handleStartGame);
-
     return () => {
-      socket.off("startGame", handleStartGame)
+      socket.off("startGame", handleStartGame);
     };
   }, [navigate, socket]);
 
   useEffect(() => {
-    const handleReceiveChallenge = ({ challenger, timer }: {challenger: string, timer: string}) => {
+    const handleReceiveChallenge = ({
+      challenger,
+      timer,
+    }: {
+      challenger: string;
+      timer: string;
+    }) => {
       setChallenger(challenger);
-      setTimer(timer); // Reset timer when a new challenge comes in
-    }
-    // Listen for incoming challenges
+      setTimer(timer);
+    };
     socket.on("receiveChallenge", handleReceiveChallenge);
-
     return () => {
-      socket.off("receiveChallenge", handleReceiveChallenge)
-    }
+      socket.off("receiveChallenge", handleReceiveChallenge);
+    };
   }, [socket]);
 
   return (
-    <div className="container flex-col flex items-center mx-auto px-4 py-8">
-      <div className="bg-[#454545] w-full max-w-[640px] h-[640px] p-12 rounded-lg shadow-xl">
-        <OnlineUsers
-          users={onlineUsers}
-          setOpponant={setOpponant}
-          setIsChallengePopUpOpen={setIsChallengePopUpOpen}
-        />
+    <div className="container flex flex-col items-center mx-auto px-4 py-8">
+      <div className="tabs flex space-x-4 mb-6">
+        <button
+          className={`tab py-2 px-4 rounded-lg text-white ${
+            activeTab === "onlineUsers"
+              ? "bg-blue-600"
+              : "bg-gray-600 hover:bg-gray-500"
+          }`}
+          onClick={() => setActiveTab("onlineUsers")}
+        >
+          Online Users
+        </button>
+        <button
+          className={`tab py-2 px-4 rounded-lg text-white ${
+            activeTab === "quickGame"
+              ? "bg-blue-600"
+              : "bg-gray-600 hover:bg-gray-500"
+          }`}
+          onClick={() => setActiveTab("quickGame")}
+        >
+          Quick Game
+        </button>
+        <button
+          className={`tab py-2 px-4 rounded-lg text-white ${
+            activeTab === "leaderboard"
+              ? "bg-blue-600"
+              : "bg-gray-600 hover:bg-gray-500"
+          }`}
+          onClick={() => setActiveTab("leaderboard")}
+        >
+          Leaderboard
+        </button>
       </div>
 
-      {/* )} */}
+      <div className="bg-gray-800 w-full max-w-[640px] h-[640px] p-8 rounded-lg shadow-xl">
+        {activeTab === "onlineUsers" && (
+          <OnlineUsers
+            users={onlineUsers}
+            setOpponant={setOpponant}
+            setIsChallengePopUpOpen={setIsChallengePopUpOpen}
+          />
+        )}
+
+        {activeTab === "quickGame" && (
+          <div className="quick-game text-center">
+            <h2 className="text-2xl font-bold text-yellow-400 mb-6">
+              Quick Game
+            </h2>
+            <h3 className="flex text-white font-bold m-2">Bullet</h3>
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+              {timerCategories
+                .find((category) => category.name === "Bullet")
+                ?.options.map((option) => (
+                  <button
+                    key={option.value}
+                    className="bg-[#454545] text-yellow-500 border border-yellow-400 font-semibold rounded-lg hover:bg-yellow-400 hover:text-black px-6 py-3 transition-all ease-in-out transform hover:scale-105"
+                    onClick={() => handleQuickPair(option.value)}
+                  >
+                    {option.label}
+                  </button>
+                ))}
+            </div>
+            <h3 className="flex text-white font-bold m-2">Blitz</h3>
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+              {timerCategories
+                .find((category) => category.name === "Blitz")
+                ?.options.map((option) => (
+                  <button
+                    key={option.value}
+                    className="bg-[#454545] text-yellow-500 border border-yellow-400 font-semibold rounded-lg hover:bg-yellow-400 hover:text-black px-6 py-3 transition-all ease-in-out transform hover:scale-105"
+                    onClick={() => handleQuickPair(option.value)}
+                  >
+                    {option.label}
+                  </button>
+                ))}
+            </div>
+            <h3 className="flex text-white font-bold m-2">Rapid</h3>
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+              {timerCategories
+                .find((category) => category.name === "Rapid")
+                ?.options.map((option) => (
+                  <button
+                    key={option.value}
+                    className="bg-[#454545] text-yellow-500 border border-yellow-400 font-semibold rounded-lg hover:bg-yellow-400 hover:text-black px-6 py-3 transition-all ease-in-out transform hover:scale-105"
+                    onClick={() => handleQuickPair(option.value)}
+                  >
+                    {option.label}
+                  </button>
+                ))}
+            </div>
+          </div>
+        )}
+
+        {activeTab === "leaderboard" && (
+          <div className="leaderboard text-center">
+            <h2 className="text-2xl font-bold text-yellow-400 mb-6">
+              Leaderboard
+            </h2>
+            <p className="text-gray-300">Coming Soon!</p>
+          </div>
+        )}
+      </div>
+
       {challenger && (
         <ChallengerPopup
           challenger={challenger}
@@ -104,6 +239,7 @@ const Home = () => {
           onDecline={handleDecline}
         />
       )}
+
       <div className="relative">
         {isChallengePopUpOpen && (
           <ChallengePopup
