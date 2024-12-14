1781efdfa81c1df2a964759081bb96d0ea91ae4e
djaafarhamri <hamridjaafar12@gmail.com>
2024-12-14 16:46:12 +0100

    Moved chess-ui code into views folder

--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-declare module '*.svg' {
-    import { FC, SVGProps } from 'react';
-    export const ReactComponent: FC<SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
-  }
